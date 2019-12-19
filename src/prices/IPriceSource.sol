3ba83d76a7a17cfc63bbd1ccb956dc19256d9fbd
luongnt95 <luongnt.58@gmail.com>
2019-12-20 00:12:34 +0700

    refactor: fix linting errors

--- src/prices/IPriceSource.sol
+++ src/prices/IPriceSource.sol
@@ -12,15 +12,35 @@ interface IPriceSource {
 
     /// @notice Return the last known price, and when it was issued
     function getPrice(address _asset) external view returns (uint price, uint timestamp);
-    function getPrices(address[] calldata _assets) external view returns (uint[] memory prices, uint[] memory timestamps);
+
+    function getPrices(
+        address[] calldata _assets
+    ) external view returns (uint[] memory prices, uint[] memory timestamps);
 
     /// @notice Get price info, and revert if not valid
     function getPriceInfo(address _asset) external view returns (uint price, uint decimals);
-    function getInvertedPriceInfo(address ofAsset) external view returns (uint price, uint decimals);
 
-    function getReferencePriceInfo(address _base, address _quote) external view returns (uint referencePrice, uint decimal);
-    function getOrderPriceInfo(address sellAsset, address buyAsset, uint sellQuantity, uint buyQuantity) external view returns (uint orderPrice);
-    function existsPriceOnAssetPair(address sellAsset, address buyAsset) external view returns (bool isExistent);
+    function getInvertedPriceInfo(
+        address ofAsset
+    ) external view returns (uint price, uint decimals);
+
+    function getReferencePriceInfo(
+        address _base,
+        address _quote
+    ) external view returns (uint referencePrice, uint decimal);
+
+    function getOrderPriceInfo(
+        address sellAsset,
+        address buyAsset,
+        uint sellQuantity,
+        uint buyQuantity
+    ) external view returns (uint orderPrice);
+
+    function existsPriceOnAssetPair(
+        address sellAsset,
+        address buyAsset
+    ) external view returns (bool isExistent);
+
     function convertQuantity(
         uint fromAssetQuantity,
         address fromAsset,
